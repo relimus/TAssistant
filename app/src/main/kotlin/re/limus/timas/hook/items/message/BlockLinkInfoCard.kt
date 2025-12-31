a44f6ce93a0861c960e26dfa6ffbf834e6dbae2c
relimus <relimus@proton.me>
2025-12-31 18:18:31 +0800

    fix: 编译 & DexFinder 错误 修复因未 import 而编译失败 ( 直接在 Github 编辑不检查这一块 ) 修复因 DexFinder 参数错误而报错

--- app/src/main/kotlin/re/limus/timas/hook/items/message/BlockLinkInfoCard.kt
+++ app/src/main/kotlin/re/limus/timas/hook/items/message/BlockLinkInfoCard.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import re.limus.timas.annotations.RegisterToUI
 import re.limus.timas.annotations.UiCategory
 import re.limus.timas.hook.base.SwitchHook
+import top.sacz.xphelper.dexkit.DexFinder
+import top.sacz.xphelper.ext.toClass
 
 // dartcv
 @RegisterToUI
@@ -16,6 +18,7 @@ object BlockLinkInfoCard: SwitchHook() {
     override fun onHook(ctx: Context, loader: ClassLoader) {
         DexFinder.findMethod {
             declaredClass = "com.tencent.qqnt.kernel.nativeinterface.LinkInfo".toClass()
+            paramCount = 5
         }.hookConstructorBefore {
             if (args[0] != null) { 
                 result = null
